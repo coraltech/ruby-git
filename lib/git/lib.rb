562c5ae36118da0a6c3d30edd8749d49ff5648ac
Adrian Webb <adrian.webb@venturiscm.com>
2013-01-25 23:22:44 -0500

    Adding an update option to the git add command.

--- lib/git/lib.rb
+++ lib/git/lib.rb
@@ -392,8 +392,9 @@ module Git
       command('config', ['--global', name, value], false)
     end
           
-    def add(path = '.')
-      arr_opts = ['--']
+    def add(path = '.', opts = {})
+      arr_opts = []
+      arr_opts << '-u' if opts[:update]
       if path.is_a?(Array)
         arr_opts += path
       else
